f2ada3e4d585a7f8b978b8421b10023a348690e6
Radoslav Stankov <rstankov@gmail.com>
2014-01-07 20:35:06 +0200

    Load options from yaml file

--- lib/setty.rb
+++ lib/setty.rb
@@ -1,5 +1,9 @@
 require "setty/version"
 
 module Setty
-  # Your code goes here...
+  def self.options_from_file(path, enviroment)
+    options = YAML.load_file path
+    enviroment_options = options[enviroment.to_s]
+    ActiveSupport::OrderedOptions[enviroment_options.symbolize_keys]
+  end
 end
--- spec/setty/base_spec.rb
+++ spec/setty/base_spec.rb
@@ -1,7 +1,24 @@
 require 'spec_helper'
+require 'tempfile'
+require 'active_support/ordered_options'
+require 'active_support/core_ext/hash/keys.rb'
 
 describe Setty do
-  it "has version" do
-    expect(Setty::VERSION).to eq '0.0.1'
+  it "can load yml file" do
+    path = generate_yaml <<-YAML
+      production:
+        enviroment: 'production'
+      test:
+        enviroment: 'test'
+    YAML
+    options = Setty.options_from_file(path, 'test')
+    expect(options.enviroment).to eq 'test'
+  end
+
+  def generate_yaml(content)
+    file = Tempfile.new('test.yml')
+    file.write content
+    file.close
+    file
   end
 end
