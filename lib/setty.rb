c6659e1b86c50c1ec8a7e30b50c99b6bc94a1a95
Radoslav Stankov <rstankov@gmail.com>
2014-01-08 14:33:31 +0200

    Extract Loader class

--- lib/setty.rb
+++ lib/setty.rb
@@ -3,12 +3,23 @@ require 'erb'
 
 module Setty
   def self.load(path, enviroment)
-    return ActiveSupport::OrderedOptions.new unless File.readable? path
+    Loader.new(path, enviroment).options
+  end
 
-    yaml_content       = ERB.new(File.read(path)).result
-    options            = YAML.load yaml_content
-    enviroment_options = options[enviroment.to_s]
+  class Loader
+    def initialize(path, enviroment)
+      @base_path  = path
+      @enviroment = enviroment
+    end
 
-    ActiveSupport::OrderedOptions[enviroment_options.symbolize_keys]
+    def options
+      return ActiveSupport::OrderedOptions.new unless File.readable? @base_path
+
+      yaml_content       = ERB.new(File.read(@base_path)).result
+      options            = YAML.load yaml_content
+      enviroment_options = options[@enviroment.to_s]
+
+      ActiveSupport::OrderedOptions[enviroment_options.symbolize_keys]
+    end
   end
 end
