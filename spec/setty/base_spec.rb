be33a18d71a7b5d7f4d0fb761dfb3ed6dc2764aa
Radoslav Stankov <rstankov@gmail.com>
2014-01-08 14:33:58 +0200

    Move to fixture files

--- spec/setty/base_spec.rb
+++ spec/setty/base_spec.rb
@@ -4,37 +4,26 @@ require 'active_support/ordered_options'
 require 'active_support/core_ext/hash/keys.rb'
 
 describe Setty do
+  def fixture(file_name)
+    File.expand_path File.join(File.dirname(__FILE__), '..', 'fixtures', file_name)
+  end
+
   describe "#load" do
     it "loads yaml depending on environment variable" do
-      path = generate_yaml <<-YAML
-      production:
-        enviroment: 'production'
-      test:
-        enviroment: 'test'
-      YAML
+      path = fixture 'settings.yml'
       options = Setty.load(path, 'test')
       expect(options.enviroment).to eq 'test'
     end
 
     it "interpolates the yaml content" do
-      path = generate_yaml <<-YAML
-      test:
-        enviroment: <%= 1 + 1 %>
-      YAML
+      path = fixture 'settings.yml'
       options = Setty.load(path, 'test')
-      expect(options.enviroment).to eq 2
+      expect(options.interpolation).to eq 2
     end
 
     it "returns an empty options for non existing yaml file" do
       options = Setty.load('non-existing', 'test')
       expect(options).to be_empty
     end
-  end
-
-  def generate_yaml(content)
-    file = Tempfile.new('test.yml')
-    file.write content
-    file.close
-    file
   end
 end
