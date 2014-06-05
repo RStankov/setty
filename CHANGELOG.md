7ba801f5a974a6608aa417505ab6f4ded96649fc
Radoslav Stankov <rstankov@gmail.com>
2014-06-05 15:59:42 +0200

    Add enviroment option to setty

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,21 @@
 # Changelog
 
+## Version 1.1 (unreleased)
+
+* Added `settings_environment` option. It defaults to `Rails.env`.
+
+  Example:
+
+    # config/application.rb
+    config.setty.settings_environment = 'custom'
+
+    # config/settings.yml
+    custom:
+      key: "value"
+
+    development:
+      key: "other value"
+
 ## Version 1.0
 
 * Initial release
--- lib/setty/railtie.rb
+++ lib/setty/railtie.rb
@@ -3,12 +3,14 @@ module Setty
     config.setty                        = ActiveSupport::OrderedOptions.new
     config.setty[:settings_path]        = 'settings'
     config.setty[:settings_object_name] = 'Settings'
+    config.setty[:settings_environment] = Rails.env
 
     initializer 'setty.load' do |app|
       path        = app.root.join 'config', app.config.setty[:settings_path]
       object_name = app.config.setty[:settings_object_name]
+      environment = app.config.setty[:settings_environment]
 
-      Object.const_set object_name, ::Setty.load(path, Rails.env)
+      Object.const_set object_name, ::Setty.load(path, environment)
     end
   end
 end
